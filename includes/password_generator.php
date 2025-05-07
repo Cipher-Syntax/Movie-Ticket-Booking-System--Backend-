fa4d8276bc44d94017a6239c1968a31afe880fb2
Justine Toong <programmerjustine@gmail.com>
2025-05-07 18:47:02 +0800

    Added some sql procedure for adding new admin

--- includes/password_generator.php
+++ includes/password_generator.php
@@ -1,8 +1,37 @@
 <?php
-    $password = "wmsu00018";
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+    require_once("../class/Connection.php");
+    require_once("../class/Database.php");
+    require_once("../class/UserRegistration.php");
+
+    $email = "admin123@gmail.com";
+    $password = "admin123";
     $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
-    echo $hashed_password;
+    // echo $hashed_password;
 
+    $checkQuery = "SELECT * FROM admin WHERE admin_email = :email";
+    $stmt = $conn->prepare($checkQuery);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    $existingUser = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if ($existingUser) {
+        echo "<script>alert('Email already exists.');</script>";
+    } 
+    else {
+        $query = "CALL addAdminAccount(:email, :hashed_password)";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':hashed_password', $hashed_password);
+        
+        if ($stmt->execute()) {
+            echo "<script>alert('Successfully added admin');</script>";
+        } else {
+            echo "<script>alert('Failed to add admin');</script>";
+        }
+    }
     
 ?>
